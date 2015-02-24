e49290143fdb6bce5a8fe670cfb4826dee0874dc
Robin Speekenbrink <robin@kingsquare.nl>
2015-02-24 18:20:03 +0100

    untested first 0.0.1 release for the dokku deployment... Will this blend? :)

--- README.md
+++ README.md
@@ -1,40 +1,31 @@
-# Heroku deployment step
+# dokku deployment step
 
-Deploy your code to Heroku. This step requires that you deploy to a Heroku deploy target.
+Deploy your code to a dokku(-alt) host. (All further mentioning of dokku also applies to dokku-alt)
 
 # Using wercker SSH key pair
 
-To push to Heroku we need to have an ssh key. We can dynamically generate a key, add the key to your repository and then remove it, during each build. But this results in an e-mail from Heroku on each deploy.
-
-To prevent this you can generate a private/public key pair on wercker and manually add the public key to Heroku.
+You should generate a private/public key pair on wercker and manually add the public key to the dokku host.
 
 - Generate a new key in wercker in the `Key management` section (`application` - `settings`).
-- Copy the public key and add it on Heroku to the `SSH Keys section` section (`account`).
-- In wercker edit the Heroku deploy target to which you would like to deploy, and add an environment variable:
+- Copy the public key and add it on the intened dokku host.
+- In wercker edit the dokku deploy target to which you would like to deploy, and add an environment variable:
     - Give the environment variable a name (remember this name, you will need it in the last step).
     - Select `SSH Key pair` as the type and select the key pair which you created earlier.
-- In the `heroku-deploy` step in your `wercker.yml` add the `key-name` property with the value you used earlier:
+- In the `dokku-deploy` step in your `wercker.yml` add the `key-name` property with the value you used earlier:
 
 ``` yaml
 deploy:
     steps:
-        - heroku-deploy:
+        - dokku-deploy:
             key-name: MY_DEPLOY_KEY
 ```
 
 In the above example the `MY_DEPLOY_KEY` should match the environment variable name you used in wercker. Note: you should not prefix it with a dollar sign or post fix it with `_PRIVATE` or `_PUBLIC`.
 
-# What's new
-
-- Remove some changing of directories
-- Mark `source_dir` as deprecated. In a future version this will be replaced with `cwd`, currently they however are not compatible.
-
 # Options
 
-* `key-name` (optional) Specify the name of the key that should be used for this deployment. If left empty, a temporary key will be created for the deployment.
-* `retry` (optional) When a deploy to Heroku fails, a new deploy is automatically performed after 5 seconds. If you want to disable this behavior, set `retry` to `false`.
-* `run` (optional) Run a command on heroku after the code is deployed succesfully. This option can be used to migrate the database for example.
-* `install-toolbelt` (optional). If set to 'true', the toolbelt will be installed. Note: specifying a run command will also install the toolbelt.
+* `key-name` (optional) Specify the name of the key that should be used for this deployment.
+* `retry` (optional) When a deploy to dokku fails, a new deploy is automatically performed after 5 seconds. If you want to disable this behavior, set `retry` to `false`.
 * `keep-repository` (optional) This will allow a user to keep the original history of the repository, speeding up deployment. **Important:** changes made during the build will not be deployed. Also keep in mind that deploying an already up to date repo will not result in an application restart. Use the `run` parameter to forcibly reload to achieve this. This feature is considered beta, expect issues. If you find one, please contact us.
 
 # Example
@@ -42,107 +33,17 @@ In the above example the `MY_DEPLOY_KEY` should match the environment variable n
 ``` yaml
 deploy:
     steps:
-        - heroku-deploy:
+        - dokku-deploy:
             key-name: MY_DEPLOY_KEY
+            dokku-host: MY_DEPLOY_KEY
+            app-name: MY_DEPLOY_KEY
 ```
-
-# Special thanks
-
-- [wernerb](https://app.wercker.com/#wernerb) for fixing submodule support and allowing the repository to be reused.
-
 # License
 
 The MIT License (MIT)
 
 # Changelog
 
-## 2.2.1
+## 0.0.1
 
-- fixed: ssh: connect to host git.heroku.com port 22: Connection timed out
-
-## 2.2.0
-
-- Remove some changing of directories
-- Mark `source_dir` as deprecated. In a future version this will be replaced with `cwd`, currently they however are not compatible.
-
-## 2.1.0
-
-- Check if API key is still valid.
-- Check if app exists on Heroku.
-- Suppress output of curl.
-
-## 2.0.2
-
-- fix: allow step to be run multiple times in a single deploy
-
-## 2.0.1
-
-- add install_toolbelt option
-
-## 2.0.0
-
-- Major refactor:
-- Only install toolbelt if needed (currently only required for running a command).
-- Fix security issue, where the host key was ignored.
-- run.sh now uses functions.
-
-## 1.0.14
-
-- Add `keep-repository` parameter.
-
-## 1.0.11
-
-- Fix pushing submodules to heroku by flattening submodules if found.
-
-## 1.0.9
-
-- Make sure git-core is installed when heroku toolbelt.
-
-## 1.0.7
-
-* Fix bugs related to the `run` parameter.
-
-## 1.0.5
-
-* Update README, to clearify using a SSH Key pair.
-
-## 1.0.4
-
-* Fixed release that also works on ruby based boxes (always installs ruby 1.9.1)
-
-## 1.0.1
-
-* fix for apt-get bug: deb files are now included [broken]
-
-## 1.0.0
-
-* Bug fixes
-
-## 0.0.9-dev
-
-* Adds `run` option.
-
-## 0.0.8
-
-* Retry on failure.
-
-## 0.0.7
-
-* Only call `heroku keys:remove` if a ephemeral key was used (wercker/step-heroku-deploy#2).
-* Update README.
-
-## 0.0.6
-
-* Fix wrong option check.
-
-## 0.0.4
-
-* Added validation to `key-name` option.
-
-## 0.0.3
-
-* Added `key-name` option.
-
-## 0.0.2
-
-* Initial release.
+* Initial release based of the excellent work already done by wercker themselves as the heroku-deploy-step.
--- run.sh
+++ run.sh
@@ -1,62 +1,63 @@
 set -e;
 
 init_wercker_environment_variables() {
-    if [ -z "$WERCKER_HEROKU_DEPLOY_KEY"  ]; then
-        if [ ! -z "$HEROKU_KEY" ]; then
-            export WERCKER_HEROKU_DEPLOY_KEY="$HEROKU_KEY";
+    if [ -z "$WERCKER_DOKKU_DEPLOY_KEY"  ]; then
+        if [ ! -z "$DOKKU_KEY" ]; then
+            export WERCKER_DOKKU_DEPLOY_KEY="$DOKKU_KEY";
         else
-            fail "Missing or empty option heroku_key. $error_suffix";
+            fail "Missing or empty option dokku_key. $error_suffix";
         fi
     fi
 
-    if [ -z "$WERCKER_HEROKU_DEPLOY_APP_NAME" ]; then
-        if [ ! -z "$HEROKU_APP_NAME" ]; then
-            export WERCKER_HEROKU_DEPLOY_APP_NAME="$HEROKU_APP_NAME";
+    if [ -z "$WERCKER_DOKKU_DEPLOY_APP_NAME" ]; then
+        if [ ! -z "$DOKKU_APP_NAME" ]; then
+            export WERCKER_DOKKU_DEPLOY_APP_NAME="$DOKKU_APP_NAME";
         else
-            fail "Missing or empty option heroku_app_name. $error_suffix";
+            fail "Missing or empty option dokku_app_name. $error_suffix";
         fi
     fi
 
-    if [ -z "$WERCKER_HEROKU_DEPLOY_USER" ]; then
-        if [ ! -z "$HEROKU_USER" ]; then
-            export WERCKER_HEROKU_DEPLOY_USER="$HEROKU_USER";
+    if [ -z "$WERCKER_DOKKU_DEPLOY_HOST" ]; then
+        if [ ! -z "$DOKKU_DEPLOY_HOST" ]; then
+            export $WERCKER_DOKKU_DEPLOY_HOST="$DOKKU_DEPLOY_HOST";
         else
-            export WERCKER_HEROKU_DEPLOY_USER="heroku-deploy@wercker.com";
+            fail "Missing or empty option dokku_deploy_host. $error_suffix";
         fi
     fi
 
-    if [ -z "$WERCKER_HEROKU_DEPLOY_SOURCE_DIR" ]; then
-        export WERCKER_HEROKU_DEPLOY_SOURCE_DIR="$WERCKER_ROOT";
-        debug "option source_dir not set. Will deploy directory $WERCKER_HEROKU_DEPLOY_SOURCE_DIR";
+    if [ -z "$WERCKER_DOKKU_DEPLOY_KEY_NAME" ]; then
+        if [ ! -z "$DOKKU_DEPLOY_KEY_NAME" ]; then
+            export $WERCKER_DOKKU_DEPLOY_KEY_NAME="$DOKKU_DEPLOY_KEY_NAME";
+        else
+            fail "Missing or empty option dokku_deploy_key_name. $error_suffix";
+        fi
+    fi
+
+    if [ -z "$WERCKER_DOKKU_DEPLOY_USER" ]; then
+        if [ ! -z "$DOKKU_USER" ]; then
+            export WERCKER_DOKKU_DEPLOY_USER="$DOKKU_USER";
+        else
+            export WERCKER_DOKKU_DEPLOY_USER="dokku";
+        fi
+    fi
+
+    if [ -z "$WERCKER_DOKKU_DEPLOY_SOURCE_DIR" ]; then
+        export WERCKER_DOKKU_DEPLOY_SOURCE_DIR="$WERCKER_ROOT";
+        debug "option source_dir not set. Will deploy directory $WERCKER_DOKKU_DEPLOY_SOURCE_DIR";
     else
-        warn "Use of source_dir is deprecated. Please make sure that you fix your Heroku deploy version on a major version."
-        debug "option source_dir found. Will deploy directory $WERCKER_HEROKU_DEPLOY_SOURCE_DIR";
+        warn "Use of source_dir is deprecated. Please make sure that you fix your dokku deploy version on a major version."
+        debug "option source_dir found. Will deploy directory $WERCKER_DOKKU_DEPLOY_SOURCE_DIR";
     fi
-}
-
-init_netrc() {
-    local username="$1";
-    local password="$2";
-
-    # echo "machine api.heroku.com" > $HOME/.netrc
-    echo "machine api.heroku.com" > $HOME/.netrc;
-    echo "  login $username" >> $HOME/.netrc;
-    echo "  password $password" >> $HOME/.netrc;
-    chmod 0600 $HOME/.netrc;
 }
 
 init_ssh() {
-    local heroku_public_key="heroku.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAu8erSx6jh+8ztsfHwkNeFr/SZaSOcvoa8AyMpaerGIPZDB2TKNgNkMSYTLYGDK2ivsqXopo2W7dpQRBIVF80q9mNXy5tbt1WE04gbOBB26Wn2hF4bk3Tu+BNMFbvMjPbkVlC2hcFuQJdH4T2i/dtauyTpJbD/6ExHR9XYVhdhdMs0JsjP/Q5FNoWh2ff9YbZVpDQSTPvusUp4liLjPfa/i0t+2LpNCeWy8Y+V9gUlDWiyYwrfMVI0UwNCZZKHs1Unpc11/4HLitQRtvuk0Ot5qwwBxbmtvCDKZvj1aFBid71/mYdGRPYZMIxq1zgP1acePC1zfTG/lvuQ7d0Pe0kaw==";
-
     mkdir -p $HOME/.ssh;
     touch $HOME/.ssh/known_hosts;
     chmod 600 $HOME/.ssh/known_hosts;
-    echo $heroku_public_key >> $HOME/.ssh/known_hosts;
 }
 
 init_git() {
     local username="$1";
-    local email="$2";
 
     if ! type git &> /dev/null; then
         debug "git not found; installing it"
@@ -68,7 +69,6 @@ init_git() {
     fi
 
     git config --global user.name "$username";
-    git config --global user.email "$email";
 }
 
 init_gitssh() {
@@ -78,28 +78,6 @@ init_gitssh() {
     echo "ssh -e none -i \"$ssh_key_path\" \$@" > $gitssh_path;
     chmod 0700 $gitssh_path;
     export GIT_SSH="$gitssh_path";
-}
-
-install_toolbelt() {
-    if ! type heroku &> /dev/null; then
-        info 'heroku toolbelt not found, starting installing it';
-
-        sudo apt-get update;
-        sudo apt-get install -y ruby1.9.1;
-        result=$(sudo dpkg -i $WERCKER_STEP_ROOT/foreman-0.60.0.deb $WERCKER_STEP_ROOT/heroku-3.2.0.deb $WERCKER_STEP_ROOT/heroku-toolbelt-3.2.0.deb);
-
-        if [[ $? -ne 0 ]]; then
-            warning $result;
-            fail 'heroku toolbelt installation failed';
-        else
-            info 'finished heroku toolbelt installation';
-        fi
-    else
-        info 'heroku toolbelt is available, and will not be installed by this step';
-    fi
-
-    debug "type heroku: $(type heroku)";
-    debug "heroku version: $(heroku --version)";
 }
 
 use_wercker_ssh_key() {
@@ -119,72 +97,26 @@ use_wercker_ssh_key() {
     chmod 0600 "$ssh_key_path";
 }
 
-use_random_ssh_key() {
-    local ssh_key_path="$1";
-
-    local ssh_key_comment="deploy-$RANDOM@wercker.com";
-
-    debug "no key-name specified, will generate key and add it to heroku";
-
-    debug 'generating random ssh key for this deploy';
-    ssh-keygen -f "$ssh_key_path" -C "$ssh_key_comment" -N '' -t rsa -q -b 4096;
-    debug "generated ssh key $ssh_key_comment for this deployment";
-    chmod 0600 "$ssh_key_path";
-
-    add_ssh_key "${ssh_key_path}.pub";
-}
-
 push_code() {
-    local app_name="$1";
+    local dokku_host="$1";
+    local dokku_user="$2";
+    local app_name="$3";
 
-    debug "starting heroku deployment with git push";
-    git push -f git@heroku.com:$app_name.git HEAD:master;
+    debug "starting dokku deployment with git push";
+    git push -f $dokku_user@$dokku_host:$app_name HEAD:master;
     local exit_code_push=$?;
 
     debug "git pushed exited with $exit_code_push";
     return $exit_code_push;
 }
 
-execute_heroku_command() {
-    local app_name="$1";
-    local command="$2";
+execute_dokku_command() {
+    local dokku_host="$1";
+    local dokku_user="$2";
+    local command="$3";
 
-    debug "starting heroku run $command";
-    heroku run "$command" --app $app_name;
-    local exit_code_run=$?;
-
-    debug "heroku run exited with $exit_code_run";
-    return $exit_code_run;
-}
-
-add_ssh_key() {
-    local public_key_path="$1";
-
-    local public_key=$(cat $public_key_path);
-
-    debug "Adding ssh key to Heroku account"
-
-    curl -n -X POST https://api.heroku.com/account/keys \
-        -H "Accept: application/vnd.heroku+json; version=3" \
-        -H "Content-Type: application/json" \
-        -d "{\"public_key\":\"$public_key\"" > /dev/null 2>&1;
-}
-
-calculate_fingerprint() {
-    local public_key_path="$1";
-
-    ssh-keygen -lf $public_key_path | awk '{print $2}';
-}
-
-remove_ssh_key() {
-    local public_key_path="$1";
-
-    local fingerprint=$(calculate_fingerprint $public_key_path);
-
-    debug "Removing ssh key from Heroku account (fingerprint: $fingerprint)"
-
-     curl -n -X DELETE https://api.heroku.com/account/keys/$fingerprint \
-        -H "Accept: application/vnd.heroku+json; version=3" > /dev/null 2>&1;
+    debug "NOT READY YET, SHOULD BE A ssh $dokku_user@$dokku_host $command?";
+    return 1;
 }
 
 use_current_git_directory() {
@@ -210,7 +142,7 @@ use_new_git_repository() {
 
     # If there is a git repository, remove it because
     # we want to create a new git repository to push
-    # to heroku.
+    # to dokku.
     if [ -d "$working_directory/.git" ]; then
         debug "found git repository in $working_directory"
         warn "Removing git repository from $working_directory"
@@ -227,120 +159,91 @@ use_new_git_repository() {
     fi
 
     # Create git repository and add all files.
-    # This repository will get pushed to heroku.
+    # This repository will get pushed to dokku.
     git init
     git add .
     git commit -m 'wercker deploy'
 }
 
 test_authentication() {
-    local app_name="$1"
+    local dokku_host="$1";
+    local dokku_user="$2";
+    local app_name="$3";
 
     set +e;
-    curl -n --fail \
-        -H "Accept: application/vnd.heroku+json; version=3" \
-        https://api.heroku.com/account > /dev/null 2>&1;
+    ssh -t $dokku_user@$dokku_host > /dev/null 2>&1;
     local exit_code_authentication_test=$?;
     set -e;
 
     if [ $exit_code_authentication_test -ne 0 ]; then
-        fail 'Unable to retrieve account information, please update your Heroku API key';
-    fi
-
-    set +e;
-    curl -n --fail \
-        -H "Accept: application/vnd.heroku+json; version=3" \
-        https://api.heroku.com/apps/$app_name > /dev/null 2>&1;
-    local exit_code_app_test=$?
-    set -e;
-
-    if [ $exit_code_app_test -ne 0 ]; then
-        fail 'Unable to retrieve application information, please check if the Heroku application still exists';
+        fail 'Unable to login using your provided credentials, please update your dokku host, username etc';
     fi
 }
 
 # === Main flow starts here ===
 ssh_key_path="$(mktemp -d)/id_rsa";
 gitssh_path="$(mktemp)";
-error_suffix='Please add this option to the wercker.yml or add a heroku deployment target on the website which will set these options for you.';
+error_suffix='Please add this option to the wercker.yml or add a dokku deployment target on the website which will set these options for you.';
 exit_code_push=0;
 exit_code_run=0;
 
 # Initialize some values
 init_wercker_environment_variables;
-init_netrc "$WERCKER_HEROKU_DEPLOY_USER" "$WERCKER_HEROKU_DEPLOY_KEY";
 init_ssh;
-init_git "$WERCKER_HEROKU_DEPLOY_USER" "$WERCKER_HEROKU_DEPLOY_USER";
+init_git "$WERCKER_DOKKU_DEPLOY_USER";
 init_gitssh "$gitssh_path" "$ssh_key_path";
 
-cd $WERCKER_HEROKU_DEPLOY_SOURCE_DIR || fail "could not change directory to source_dir \"$WERCKER_HEROKU_DEPLOY_SOURCE_DIR\""
+cd $WERCKER_DOKKU_DEPLOY_SOURCE_DIR || fail "could not change directory to source_dir \"$WERCKER_DOKKU_DEPLOY_SOURCE_DIR\""
 
 # Test credentials
-test_authentication "$WERCKER_HEROKU_DEPLOY_APP_NAME";
+test_authentication "$WERCKER_DOKKU_DEPLOY_HOST" "$WERCKER_DOKKU_DEPLOY_USER" "$WERCKER_DOKKU_DEPLOY_APP_NAME";
 
-# Check if the user supplied a wercker key-name
-if [ -n "$WERCKER_HEROKU_DEPLOY_KEY_NAME" ]; then
-    use_wercker_ssh_key "$ssh_key_path" "$WERCKER_HEROKU_DEPLOY_KEY_NAME";
-else
-    use_random_ssh_key "$ssh_key_path";
-fi
+use_wercker_ssh_key "$ssh_key_path" "$WERCKER_DOKKU_DEPLOY_KEY_NAME";
 
 # Then check if the user wants to use the git repository or use the files in the source directory
-if [ "$WERCKER_HEROKU_DEPLOY_KEEP_REPOSITORY" == "true" ]; then
-    use_current_git_directory "$WERCKER_HEROKU_DEPLOY_SOURCE_DIR" "$WERCKER_GIT_BRANCH";
+if [ "$WERCKER_DOKKU_DEPLOY_KEEP_REPOSITORY" == "true" ]; then
+    use_current_git_directory "$WERCKER_DOKKU_DEPLOY_SOURCE_DIR" "$WERCKER_GIT_BRANCH";
 else
-    use_new_git_repository "$WERCKER_HEROKU_DEPLOY_SOURCE_DIR";
+    use_new_git_repository "$WERCKER_DOKKU_DEPLOY_SOURCE_DIR";
 fi
 
 # Try to push the code
 set +e;
-push_code "$WERCKER_HEROKU_DEPLOY_APP_NAME";
+push_code "$WERCKER_DOKKU_DEPLOY_HOST" "$WERCKER_DOKKU_DEPLOY_USER" "$WERCKER_DOKKU_DEPLOY_APP_NAME";
 exit_code_push=$?
 set -e;
 
 # Retry pushing the code, if the first push failed and retry was not disabled
 if [ $exit_code_push -ne 0 ]; then
-    if [ "$WERCKER_HEROKU_DEPLOY_RETRY" == "false" ]; then
+    if [ "$WERCKER_DOKKU_DEPLOY_RETRY" == "false" ]; then
         info "push failed, not going to retry";
     else
         info "push failed, retrying push in 5 seconds";
         sleep 5;
 
         set +e;
-        push_code "$WERCKER_HEROKU_DEPLOY_APP_NAME";
+        push_code "$WERCKER_DOKKU_DEPLOY_HOST" "$WERCKER_DOKKU_DEPLOY_USER" "$WERCKER_DOKKU_DEPLOY_APP_NAME";
         exit_code_push=$?
         set -e;
     fi
 fi
 
-
-if [ "$WERCKER_HEROKU_DEPLOY_INSTALL_TOOLBELT" == "true" -o -n "$WERCKER_HEROKU_DEPLOY_RUN" ]; then
-    set +e;
-    install_toolbelt;
-    set -e;
-fi
-
 # Run a command, if the push succeeded and the user supplied a run command
-if [ -n "$WERCKER_HEROKU_DEPLOY_RUN" ]; then
+if [ -n "$WERCKER_DOKKU_DEPLOY_RUN" ]; then
     if [ $exit_code_push -eq 0 ]; then
         set +e;
-        execute_heroku_command "$WERCKER_HEROKU_DEPLOY_APP_NAME" "$WERCKER_HEROKU_DEPLOY_RUN";
+        execute_dokku_command "$WERCKER_DOKKU_DEPLOY_HOST" "$WERCKER_DOKKU_DEPLOY_USER" "$WERCKER_DOKKU_DEPLOY_RUN";
         exit_code_run=$?
         set -e;
     fi
 fi
 
-# Remove a auto generated key (assuming we generated a public key at ${ssh_key_path}.pub)
-if [ -z "$WERCKER_HEROKU_DEPLOY_KEY_NAME" ]; then
-    remove_ssh_key "${ssh_key_path}.pub";
-fi
-
 if [ $exit_code_run -ne 0 ]; then
-    fail 'heroku run failed';
+    fail 'dokku run failed';
 fi
 
 if [ $exit_code_push -eq 0 ]; then
-    success 'deployment to heroku finished successfully';
+    success 'deployment to dokku finished successfully';
 else
-    fail 'git push to heroku failed';
+    fail 'git push to dokku failed';
 fi
